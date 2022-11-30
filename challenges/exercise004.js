fc111d3999117899dce07abe15ac12675f4f9dde
Matthew Morgan <matthew@hidlen.com>
2022-11-30 17:32:09 +0000

    Completes the fourth set of exercise with map/filter

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,83 +1,41 @@
 export function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  let result = [];
-  nums.forEach((e) => {
-    if (e < 1) {
-      result.push(e);
-    }
-  });
-  return result;
+  return nums.filter(num => num < 1);
 }
 
 export function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  let result = [];
-  names.forEach((e) => {
-    if (e.charAt(0) === char) {
-      result.push(e);
-    }
-  });
-  return result;
+  return names.filter(name => name.charAt(0) == char);
 }
 
 export function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  let result = [];
-  words.forEach((e) => {
-    if (e.substring(0, 3) == 'to ') {
-      result.push(e);
-    }
-  });
-  return result;
+  return words.filter(word => word.substring(0, 3) == 'to ');
 }
 
 export function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  let result = [];
-  nums.forEach((e) => {
-    if (Number.isInteger(e)) {
-      result.push(e);
-    }
-  });
-  return result;
+  return nums.filter(num => Number.isInteger(num));
 }
 
 export function getCities(users) {
   if (!users) throw new Error("users is required");
-  let result = [];
-  users.forEach((e) => {
-    result.push(e.data.city.displayName);
-  });
-  return result;
+  return users.map(user => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  let result = [];
-  nums.forEach((e) => {
-    result.push(Math.round((Math.sqrt(e) * 100)) / 100);
-  });
-  return result;
+  return nums.map(num => (Math.round((Math.sqrt(num) * 100)) / 100));
 }
 
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  let result = [];
-  sentences.forEach((e) => {
-    if (e.toLowerCase().includes(str.toLowerCase())) {
-      result.push(e);
-    }
-  });
-  return result;
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()));
 }
 
 export function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  let result = [];
-  triangles.forEach((e) => {
-    result.push(Math.max(...e));
-  });
-  return result;
+  return triangles.map(triangle => Math.max(...triangle));
 }
