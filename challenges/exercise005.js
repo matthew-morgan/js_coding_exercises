c8fb061b0ea8b95225bece9d5b74db9c20c6be32
Matthew Morgan <matthew@hidlen.com>
2022-11-30 22:10:37 +0000

    Completes the fifth set of exercises

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,36 +1,59 @@
 export const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  if (!nums.includes(n) || nums.indexOf(n) == nums.length - 1) {
+    return null;
+  } else {
+    return (nums.find((num, index, self) => self[index - 1] === n));
+  }
 };
 
 export const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  return {
+    1: str.split("").filter(e => e == 1).length,
+    0: str.split("").filter(e => e == 0).length
+  }
 };
 
 export const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  return (parseInt(Array.from(String(n), Number).reverse().join("")));
 };
 
 export const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  return arrs.flat().reduce((a, b) => a + b);
 };
 
 export const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (arr.length < 2) {
+    return arr;
+  }
+  else {
+    let first = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = first;
+    return arr;
+  }
 };
 
 export const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  return Object.values(haystack).some(value => String(value).toLowerCase().includes(searchTerm.toLowerCase()));
 };
 
 export const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let frequencyMap = {};
+  str.replace(/[^a-z0-9 ]/gi, '').toLowerCase().split(" ").forEach(word => {
+    if (frequencyMap[word]) {
+      frequencyMap[word]++;
+    } else {
+      frequencyMap[word] = 1;
+    }
+  });
+  return frequencyMap;
 };
