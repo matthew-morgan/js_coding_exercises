fd0bc92bf969447a029e21a175f9e7085673c465
Matthew Morgan <matthew@hidlen.com>
2022-11-30 14:07:24 +0000

    Completes the third set of exercises

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,32 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map((num) => num * num);
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  return words.map((word, index) => {
+    if (index === 0) {
+      return word;
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+  }).join("");
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return people.reduce((acc, person) => acc + person.subjects.length, 0);
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  return menu.some((item) => item.ingredients.includes(ingredient));
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  return arr1.filter(num => arr2.includes(num)).filter((num, index, self) => self.indexOf(num) === index).sort();
 }
